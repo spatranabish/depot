6350acff7ad903fb6d1ba919e36eb22a29083e2c
spatranabish <sathipatranabish@gmail.com>
2012-12-29 11:12:07 +0530

    gemfile modified

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '7b85e6d55f811a93a621536ab50e343ff212c818f3ad9a69500e7f267dde31f71f004e44fdbfc9138b45f921667bce68367da789710d6928275909259e270874'
