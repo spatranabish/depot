a002dcc09e71934beca0efe5714c6b9aadefe559
spatranabish <sathipatranabish@gmail.com>
2012-12-30 09:12:53 +0530

    finished till ajax cart

---
+++ app/controllers/carts_controller.rb
@@ -0,0 +1,49 @@
+class CartsController < ApplicationController
+
+  def index
+    @carts = Cart.all
+  end
+
+  def show
+    begin
+      @cart = Cart.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      logger.error "Attempt to access invalid cart #{params[:id]}"
+      redirect_to store_url, :notice => 'Invalid cart'
+    end
+  end
+  
+  def new
+    @cart = Cart.new
+  end
+  
+  def edit
+    @cart = Cart.find(params[:id])
+  end
+  
+  def create
+    @cart = Cart.new(params[:cart])
+    if @cart.save
+      redirect_to(@cart, notice: 'Cart was successfully created.')
+    else
+      render action: "new" 
+    end
+  end
+  
+  def update
+    @cart = Cart.find(params[:id])
+    if @cart.update_attributes!(params[:cart])
+      redirect_to(@cart, notice: 'Cart was successfully updated.') 
+    else
+      render action: "edit" 
+    end
+  end
+  
+  def destroy
+    @cart = Cart.find(params[:id])
+    @cart.destroy
+    session[:cart_id] = nil
+    redirect_to store_url
+  end
+  
+end
